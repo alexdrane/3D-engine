0a029c92e91b37fea627432aab8c9a0fe0218ef2
Alex Drane <alexdrane04@gmail.com>
2019-07-15 21:44:29 +0100

    Added seperate run programs to main

---
+++ Run.py
@@ -0,0 +1,4 @@
+from Main import *
+WIDTH = 1800
+HEIGHT = 1000
+main(WIDTH,HEIGHT)
---
+++ RunAdjust.py
@@ -0,0 +1,4 @@
+from Main import *
+WIDTH = int(input("WIDTH  "))
+HEIGHT = int(input("HEIGHT  "))
+main(WIDTH,HEIGHT)
